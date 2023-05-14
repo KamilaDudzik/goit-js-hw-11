39dece4c95432d24cfe5657cd43224a5f5927763
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-05-14 20:08:21 +0200

    Changed whole function structure

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,11 +1,25 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = "https://pixabay.com/api/";
-const key = "36354038-aa1773e88ccb2b59a1c57888b";
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '36354038-aa1773e88ccb2b59a1c57888b';
+const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    key: KEY,
+    per_page: 40,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  }
+});  
 
-export const fetchImages = async () => {
-    const response = await axios.get(
-        `?key=${key}&image_type=photo&orientation=horizontal&safesearch=true`
-    );
-    return response;
-}
+export const fetchImages = async (valueInput, page) => {
+  try {
+    const { data } = await api.get("", {
+      params: { page: page, q: valueInput },
+    });
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
